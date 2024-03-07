fd8e9a9c889221dc664a5a6ec6105d2ccc366e01
John Zeng <johnzengzy@outlook.com>
2024-03-07 12:32:10 +0800

    add mutagen shortcuts

--- zsh/.oh-my-zsh/custom/scripts.zsh
+++ zsh/.oh-my-zsh/custom/scripts.zsh
@@ -7,3 +7,20 @@ lint_airflow () {
     black . && isort . && flake8 .
 }
 
+
+mutagen-sync-src () {
+  mutagen sync create ~/Desktop/src/$1 dev:~/src/$1
+}
+
+mutagen-kill-all () {
+  mutagen sync terminate --all
+}
+
+mutagen-pause-all () {
+  mutagen sync pause --all
+}
+
+mutagen-resume-all () {
+  mutagen sync resume --all
+}
+
